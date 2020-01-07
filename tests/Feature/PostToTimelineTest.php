8c8684c1422ced1bda89271a61cd8efb43c01de2
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-07 18:26:37 +0300

    first simple test, built post model controller

--- tests/Feature/PostToTimelineTest.php
+++ tests/Feature/PostToTimelineTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use App\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,6 +20,21 @@ class PostToTimelineTest extends TestCase
    /** @test */
    public function a_user_can_post_a_text_post()
    {
-       
+       $this->withoutExceptionHandling();
+
+       $this->actingAs($user = factory(User::class)->create(), 'api');
+
+       $response = $this->post('/api/posts', [
+            'data' => [
+                'type' => 'posts',
+                'attributes' => [
+                    'body' => 'Testing Body'
+                ]
+            ]
+       ]);
+
+       $post = Post::first();
+
+       $response->assertStatus(201);
    }
 }

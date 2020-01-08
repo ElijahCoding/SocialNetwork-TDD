ebd79191439e3c469e254b0fa78d04a596920b1f
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-08 15:51:57 +0300

    UI design downloaded

---
+++ resources/js/views/NewsFeed.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex flex-col items-center py-4">
+        <NewPost />
+
+        <Post />
+    </div>
+</template>
+
+<script>
+    import NewPost from '../../../../../../Downloads/code (1)/1. Introduction & Downloadable Files/design-assets-components-pack1/components/NewPost';
+    import Post from '../../../../../../Downloads/code (1)/1. Introduction & Downloadable Files/design-assets-components-pack1/components/Post';
+
+    export default {
+        name: "NewsFeed",
+
+        components: {
+            NewPost,
+            Post,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

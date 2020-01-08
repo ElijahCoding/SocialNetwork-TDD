546392452d3f9fe672d5a4250481b0cbfc543f9a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-08 18:36:00 +0300

    user profile

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,9 @@
 use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->group(function () {
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    Route::get('/posts', 'PostController@index');
-    Route::post('/posts', 'PostController@store');
+    Route::apiResources([
+        '/posts' => 'PostController',
+        '/users' => 'UserController',
+    ]);
 });
 

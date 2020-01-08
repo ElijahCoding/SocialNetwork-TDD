ad5c22ab620ecaffe374e0f9514f416515fee619
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-08 16:17:17 +0300

    bug fixed

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Start from './views/Start';
+import NewsFeed from "./views/NewsFeed";
 Vue.use(VueRouter);
 
 export default new VueRouter({
@@ -8,8 +8,8 @@ export default new VueRouter({
 
     routes: [
         {
-            path: '/', name: 'home', component: Start,
-            meta: { title: 'Start' }
+            path: '/', name: 'home', component: NewsFeed,
+            meta: { title: 'News Feed' }
         },
     ]
 });
--- resources/js/views/NewsFeed.vue
+++ resources/js/views/NewsFeed.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-    import NewPost from '../../../../../../Downloads/code (1)/1. Introduction & Downloadable Files/design-assets-components-pack1/components/NewPost';
-    import Post from '../../../../../../Downloads/code (1)/1. Introduction & Downloadable Files/design-assets-components-pack1/components/Post';
+    import NewPost from '../components/NewPost';
+    import Post from '../components/Post';
 
     export default {
         name: "NewsFeed",

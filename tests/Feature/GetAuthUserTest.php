9245e52a4862b9a5ec36618e258a62023c6a183a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-09 08:57:42 +0300

    fetch auth user

---
+++ tests/Feature/GetAuthUserTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetAuthUserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function authenticated_user_can_be_fetched()
+    {
+        
+    }
+}

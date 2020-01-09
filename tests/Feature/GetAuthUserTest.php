5a952dbf0de9856bba69bb1e96adeb1909cd795f
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-09 09:23:22 +0300

    get auth user

--- tests/Feature/GetAuthUserTest.php
+++ tests/Feature/GetAuthUserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -12,6 +13,23 @@ class GetAuthUserTest extends TestCase
     /** @test */
     public function authenticated_user_can_be_fetched()
     {
-        
+        $this->withoutExceptionHandling();
+
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+
+        $response = $this->get('/api/auth-user');
+
+        $response->assertStatus(200)
+                ->assertJson([
+                   'data' => [
+                       'user_id' => $user->id,
+                       'attributes' => [
+                           'name' => $user->name
+                       ]
+                   ],
+                    'links' => [
+                        'self' => url('/users/' . $user->id),
+                    ]
+                ]);
     }
 }

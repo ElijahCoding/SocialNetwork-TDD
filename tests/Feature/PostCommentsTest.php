4ed26ce6d7caf8f20f3d62aaf2d9f8cfc00568ec
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-11 16:39:04 +0300

    a user can comment a post , done

--- tests/Feature/PostCommentsTest.php
+++ tests/Feature/PostCommentsTest.php
@@ -13,12 +13,15 @@ class PostCommentsTest extends TestCase
     /** @test */
     public function a_user_can_comment_on_a_post()
     {
+        $this->withoutExceptionHandling();
+
         $this->actingAs($user = factory(User::class)->create(), 'api');
         $post = factory(Post::class)->create(['id' => 123]);
 
-        $response = $this->post('/api/posts/' . $post->id . '/comments', [
+        $response = $this->post('/api/posts/' . $post->id . '/comment', [
             'body' => 'A great comment here.',
-        ])->assertStatus(200);
+        ])
+            ->assertStatus(200);
 
         $comment = Comment::first();
 
@@ -27,7 +30,32 @@ class PostCommentsTest extends TestCase
         $this->assertEquals($post->id, $comment->post_id);
         $this->assertEquals('A great comment here.', $comment->body);
         $response->assertJson([
-
+            'data' => [
+                [
+                    'data' => [
+                        'type' => 'comments',
+                        'comment_id' => 1,
+                        'attributes' => [
+                            'commented_by' => [
+                                'data' => [
+                                    'user_id' => $user->id,
+                                    'attributes' => [
+                                        'name' => $user->name
+                                    ]
+                                ]
+                            ],
+                            'body' => 'A great comment here.',
+                            'commented_at' => $comment->created_at->diffForHumans(),
+                        ]
+                    ],
+                    'links' => [
+                        'self' => url('/posts/123'),
+                    ]
+                ]
+            ],
+            'links' => [
+                'self' => url('/posts'),
+            ]
         ]);
     }
 }
